3b822f5e2ea4e7d9b7b52b418b214ee11798d780
Felipe Vilhalva <84108986+rrozdoce@users.noreply.github.com>
2021-12-19 21:52:24 -0400

    Update pointer_13.c

--- Aulas/Ponteiros/pointer_13.c
+++ Aulas/Ponteiros/pointer_13.c
@@ -4,13 +4,20 @@
 
 void copiarString(char *copiarDaqui, char *colarAqui)
 {
+    while (*copiarDaqui != '\0')
+    {
+        *colarAqui = *copiarDaqui;
+        ++copiarDaqui;
+        ++colarAqui;
+    }
+    *colarAqui = '\0';
 }
 
 int main(void)
 {
     void copiarString(char *copiarDaqui, char *colarAqui);
 
-    char string1[] = "Pão com mortadela";
+    char string1[] = "Pao com mortadela";
     char string2[20];
 
     copiarString(string1, string2);
